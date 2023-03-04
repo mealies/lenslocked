be0f440a8538329b1ef965c8d4701f2a5133d645
Andrew <andrew@thatopsguy.com>
2023-03-04 17:33:24 +0000

    Add contact page and route
    Adding a new stub contact page and route

--- main.go
+++ main.go
@@ -10,8 +10,14 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Contact Page</h1><p>To get in touch, email me at "+
+		"<a href=\"mailto:jon@calhoun.io\">jon@calhoun.io</a>.")
+}
+
 func main() {
 	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on :3000")
 	http.ListenAndServe("localhost:3000", nil)
 }
